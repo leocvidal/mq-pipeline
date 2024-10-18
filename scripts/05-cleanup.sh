51d95e59ccfcbfa297f70b8ced9e7cfac56a6f0e
leocvidal <leo.vidal@au1.ibm.com>
2024-10-18 16:11:07 +1100

    Create 05-cleanup.sh

---
+++ scripts/05-cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#******************************************************************************
+
+oc delete queuemanager "$1" -n ${2}
+export routename="sni-${1}chl-ibm-mq-qm"
+oc delete route $routename -n ${2}
+
+export pvc1name="data-${1}-ibm-mq-0"
+oc delete pvc $pvc1name -n ${2}
+
+export pvc2name="${1}-ibm-mq-persisted-data"
+oc delete pvc $pvc2name -n ${2}
+
+oc delete project ${2}
